fc6a058bca1678dade78bd4d8a57b43579b72aa0
éris de sena <eloisa.silvestre@etec.sp.gov.br>
2024-05-06 16:35:20 -0300

    escrita do código e tentativa de commit

---
+++ sub.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main () {
+
+int numero1;
+int numero2;
+int total;
+
+puts("========== calculadora de subtração ==========");
+puts("Insira o primeiro valor:");
+scanf("%d",&numero1);
+puts("Insira o valor a ser subtraído:");
+scanf("%d", &numero2);
+resultado= numero1-numero2;
+printf("O número %d subtraído por %d resulta em: %d\n", numero1, numero2, total);
+puts("Obrigado por usar a calculadora =D");
+
+
+}
